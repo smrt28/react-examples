6e77bd4494a0cf2846fe2c0eb5c68a78d1908106
Ondrej Holecek <ondrej.holecek@zoomint.com>
2017-02-17 11:27:47 +0100

    card

--- src/app/Example.js
+++ src/app/Example.js
@@ -1,5 +1,99 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+
+const style = {
+  height: 200,
+  width: 200,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-flex'
+};
+
+const PaperExampleSimple = () => (
+  <div>
+    <Paper style={style} zDepth={5} > Ahoj jasdfhjka shdjashd flaks </Paper>
+    <Paper style={style} zDepth={5}> Ahoj </Paper>
+    <Paper style={style} zDepth={5}> 
+Lorem Ipsum is simply dummy text of the printing 
+    </Paper>
+  </div>
+);
+
+
+
+
+class CardExampleControlled extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
+
+  handleToggle = (event, toggle) => {
+    this.setState({expanded: toggle});
+  };
+
+  handleExpand = () => {
+    this.setState({expanded: true});
+  };
+
+  handleReduce = () => {
+    this.setState({expanded: false});
+  };
+
+  render() {
+    return (
+<div style={{width: 400}}>
+      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+        <CardHeader
+          title="URL Avatar"
+          subtitle="Subtitle"
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText>
+          <Toggle
+            toggled={this.state.expanded}
+            onToggle={this.handleToggle}
+            labelPosition="right"
+            label="This toggle controls the expanded state of the component."
+          />
+        </CardText>
+        <CardMedia
+          expandable={true}
+          overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+        >
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" expandable={true} />
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+          <FlatButton label="Expand" onTouchTap={this.handleExpand} />
+          <FlatButton label="Reduce" onTouchTap={this.handleReduce} />
+        </CardActions>
+      </Card>
+</div>
+    );
+  }
+}
+
+
+
 
 const AppBarExampleIcon = () => (
         <div>
@@ -8,6 +102,9 @@ const AppBarExampleIcon = () => (
         iconClassNameRight="muidocs-icon-navigation-expand-more"
         />
         AHOJ!
+        <PaperExampleSimple />
+
+<CardExampleControlled/>
         </div>
         );
 
